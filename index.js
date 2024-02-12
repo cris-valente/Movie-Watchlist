4924c5d39b56932f3a10ef551c7d797fc0204032
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-02-12 21:41:01 +0100

    add to watchlist

--- index.js
+++ index.js
@@ -5,25 +5,39 @@ const addToWatchlistBtn = document.querySelectorAll('.add-to-watchlist')
 const watchlistMoviesEl = document.getElementById('watchlist-listed-movies')
 
 let moviesArr = []
-
-let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+// JSON.parse(localStorage.getItem("watchlist")) || 
+let watchlist =[];
 
 if (searchBtn) {searchBtn.addEventListener('click', searchMovies)}
 
+// document.addEventListener('click', e => {
+    
+//     if (e.target.dataset.id) {
+//     // if (!watchlist.includes(localStorage.getItem(e.target.dataset.id))) {
+//     console.log(e.target.dataset.id)
+//     watchlist.push(localStorage.getItem(e.target.dataset.id))
+//     // }
+//     localStorage.setItem('watchlist', JSON.stringify(watchlist))
+//     console.log(localStorage.getItem('watchlist'))
+//     console.log(watchlist)
+//     }
+    
+// })
+
 document.addEventListener('click', e => {
-    
-    if (e.target.dataset.id) {
-    // if (!watchlist.includes(localStorage.getItem(e.target.dataset.id))) {
-    console.log(e.target.dataset.id)
-    watchlist.push(localStorage.getItem(e.target.dataset.id))
-    // }
-    localStorage.setItem('watchlist', JSON.stringify(watchlist))
-    console.log(localStorage.getItem('watchlist'))
-    console.log(watchlist)
+    if(e.target.dataset.movie === "movie" && !watchlist.includes(e.target.dataset.id)) {
+        watchlist.push(e.target.dataset.id)
+        watchlistArrToLS()
     }
-    
 })
 
+function watchlistArrToLS() {
+    watchlist.forEach( (movie, index) => {
+        movie = JSON.stringify(movie)
+        localStorage.setItem(index, movie)
+    })
+    
+}
 
 function searchMovies() {
     fetch(`https://www.omdbapi.com/?apikey=a2116ba9&s=${movieTitleSearch.value}`)
@@ -31,13 +45,15 @@ function searchMovies() {
         .then(data => {
 
             moviesArr = data.Search.map(movie => movie.imdbID)
-            renderMovies()
+            renderWatchlist()
         })
 }
 
-function renderMovies(){
+function renderWatchlist(){
+    
     moviesList.innerHTML = ''
-    localStorage.removeItem('lastSearch')
+    // lastSearch = ''
+    // localStorage.removeItem('lastSearch')
     moviesArr.forEach(id => {
     
         fetch(`https://www.omdbapi.com/?apikey=a2116ba9&i=${id}`)
@@ -47,10 +63,12 @@ function renderMovies(){
                 <div class="movie">
                 
                     <img class="poster" src="${data.Poster}">
-                    <span class="title">${data.Title}</span>
-                    <span class="rating">
-                        <span>⭐</span>${data.imdbRating}
+                    <span class="title">${data.Title}
+                        <span class="rating">
+                            <img src="./images/star.svg"> ${data.imdbRating}
+                        </span>
                     </span>
+                    
 
                     <span class="runtime">
                         ${data.Runtime}
@@ -60,20 +78,29 @@ function renderMovies(){
                     </span>
                     <span class="add">
                         <button 
+                        id="add-to-watchlist-${id}"
                         class="add-to-watchlist"
-                        data-id="${id}">+Watchlist
-                        </button>
+                        data-movie="movie"
+                        data-id="${id}">Watchlist</button>
                     </span>
 
                     <span class="plot">${data.Plot}</span>
                 </div>
                 `
-                
-                localStorage.setItem(`${id}`, JSON.stringify(data))
-            }
-        )
-        
-    }) 
+                // <img src="images/add.svg">
+                // lastSearch += JSON.stringify(data)
+                // localStorage.setItem(`${id}`, JSON.stringify(data))
+            //    return data
+            })
+    })
 }
 
+// function lastSearchToSessionStorage(){
+//     sessionStorage.setItem('lastSearch', renderMovies())
+// }
+
+
+// function renderSessionSearch(){
+//     moviesList.innerHTML = sessionStorage.getItem()
+// }
 
--- watchlist.js
+++ watchlist.js
@@ -1,29 +1,66 @@
 const watchlistMoviesEl = document.getElementById('watchlist-listed-movies')
+const search = document.getElementsByClassName('internal-link')
+
+let watchlist = []
+
+document.addEventListener('click', e => {
+    if(e.target.dataset.movie === "movie") {
+        localStorage.removeItem(e.target.dataset.id)
+        watchlist.splice(e.target.dataset.id, 1)
+        renderWatchlist()
+    }
+})
 
 function renderWatchlist(){
-    const data = JSON.parse(localStorage.getItem('watchlist'))
-    console.log(data)
-    for (let movie of data) {
-        watchlistMoviesEl.innerHTML += `
-        <div class="movie">
-        
-            <img class="poster" src="${movie.Poster}">
-            <span class="title">${data.Title}</span>
-            <span class="rating">
-                <span>⭐</span>${data.imdbRating}
-            </span>
 
-            <span class="runtime">
-                ${data.Runtime}
-            </span>
-            <span class="genre">
-                ${data.Genre}
-            </span>
-            <span class="plot">${data.Plot}</span>
-        </div>
-        `
-    }
+    watchlistMoviesEl.innerHTML = ""
+
+
+    watchlist.forEach((id, index) => {
+        fetch(`https://www.omdbapi.com/?apikey=a2116ba9&i=${id}`)
+        
+            .then(res => res.json())
+            .then(data => {
+                if (data.Response == "True"){
+                watchlistMoviesEl.innerHTML += `
+                <div class="movie">
+                
+                    <img class="poster" src="${data.Poster}">
+                    <span class="title">${data.Title}
+                        <span class="rating">
+                            <img src="./images/star.svg"> ${data.imdbRating}
+                        </span>
+                    </span>
+                    
+                    <span class="runtime">
+                        ${data.Runtime}
+                    </span>
+                    <span class="genre">
+                        ${data.Genre}
+                    </span>
+                    <span class="delete">
+                        <button 
+                        id="delete-from-watchlist-${id}"
+                        class="delete-from-watchlist"
+                        data-movie="movie"
+                        data-id="${index}">Remove</button>
+                    </span>
+
+                    <span class="plot">${data.Plot}</span>
+                </div>
+                `
+            }})
+    })
 }
-window.onload = function () {
-    renderWatchlist();
-};
+
+
+function watchlistToLS() {
+    Object.keys(localStorage).forEach(key => {
+        watchlist.push(JSON.parse(localStorage.getItem(key)))
+      });
+}
+watchlistToLS()
+renderWatchlist()
+// function lastSearchToSessionStorage(){
+//     sessionStorage.setItem('lastSearch', renderMovies())
+// }
